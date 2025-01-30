fb74cdd2bd0e9a03834836bcc13fd3589a2b6a29
Popoy-Dev <popoykua28@gmail.com>
2025-01-30 20:46:54 +0800

    ColombiaSAS navigation group

--- app/Filament/Clusters/IntermedianoColombiaSAS.php
+++ app/Filament/Clusters/IntermedianoColombiaSAS.php
@@ -7,6 +7,6 @@ use Filament\Clusters\Cluster;
 class IntermedianoColombiaSAS extends Cluster
 {
     protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
-    protected static ?string $navigationGroup = 'Companies';
+    protected static ?string $navigationGroup = 'Intermediano Group Companies';
     protected static ?int $navigationSort = 5;
 }
