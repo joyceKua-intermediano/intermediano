854606f3b9ac13e356b4e0b160c24f9806a29cd9
Popoy-Dev <popoykua28@gmail.com>
2025-02-07 00:50:15 +0800

    stafftracker partner_id nullable

--- database/migrations/2025_01_29_145741_create_staff_trackers_table.php
+++ database/migrations/2025_01_29_145741_create_staff_trackers_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->foreignId('industry_field_id')->nullable()->constrained()->cascadeOnDelete();
             $table->foreignId('intermediano_company_id')->constrained()->cascadeOnDelete();
             $table->foreignId('company_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('partner_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('partner_id')->nullable()->constrained()->cascadeOnDelete();
             $table->string('position');
             $table->date('start_date');
             $table->date('end_date');
