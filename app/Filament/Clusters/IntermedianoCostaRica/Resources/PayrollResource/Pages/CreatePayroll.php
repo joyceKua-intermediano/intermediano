d37440bf0e8ef7a6cef22851f6c5cbf956616dfc
Popoy-Dev <popoykua28@gmail.com>
2025-02-10 22:00:45 +0800

    Costa Rica: Payroll Resource

---
+++ app/Filament/Clusters/IntermedianoCostaRica/Resources/PayrollResource/Pages/CreatePayroll.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Clusters\IntermedianoCostaRica\Resources\PayrollResource\Pages;
+
+use App\Filament\Clusters\IntermedianoCostaRica\Resources\PayrollResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreatePayroll extends CreateRecord
+{
+    protected static string $resource = PayrollResource::class;
+}
