b3fe799ff8ca1b5de3c5d048a0366282c2c4cb74
Popoy-Dev <popoykua28@gmail.com>
2025-02-11 21:06:20 +0800

    Payroll cost model and migration

---
+++ app/Models/PayrollCost.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class PayrollCost extends Model
+{
+
+    use SoftDeletes;
+
+    protected $fillable = [
+        'medical_insurance'    
+    ];
+
+    public function quotation(): BelongsTo 
+    {
+        return $this->belongsTo(Quotation::class);
+    }
+}
