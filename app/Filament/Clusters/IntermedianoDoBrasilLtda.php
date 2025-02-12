d8ba6af73de4ee2c86442a9b957fc0bb2a56d53c
Popoy-Dev <popoykua28@gmail.com>
2025-02-12 22:54:19 +0800

    Intermediano change label in menu

--- app/Filament/Clusters/IntermedianoDoBrasilLtda.php
+++ app/Filament/Clusters/IntermedianoDoBrasilLtda.php
@@ -9,4 +9,6 @@ class IntermedianoDoBrasilLtda extends Cluster
     protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
     protected static ?string $navigationGroup = 'Intermediano Group Companies';
     protected static ?int $navigationSort = 6;
+    protected static ?string $navigationLabel = 'Intermediano do Brasil Ltda';
+
 }
