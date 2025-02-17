c37d339a3f60005ad2f8ab7a3e85e1e4caedf916
Popoy-Dev <popoykua28@gmail.com>
2025-02-17 21:03:41 +0800

    Costa: modify QuotationHelper

--- app/Helpers/Quotation/CostaRicaQuotationHelper.php
+++ app/Helpers/Quotation/CostaRicaQuotationHelper.php
@@ -52,7 +52,7 @@ if (!function_exists('calculateCostaRicaQuotation')) {
         // end of accumulated provision
 
         $subTotalGrossPayroll = $totalGrossIncome + $provisionsTotal + $payrollCostsTotal;
-        $fee = $record->is_fix_fee ? $record->fee : $subTotalGrossPayroll * ($record->fee / 100) ;
+        $fee = $record->is_fix_fee ? $record->fee * $record->exchange_rate : $subTotalGrossPayroll * ($record->fee / 100) ;
         $bankFee = $record->bank_fee * $record->exchange_rate;
         $subTotal = $subTotalGrossPayroll + $fee + $bankFee;
         $municipalTax = 0 * $subTotal;
