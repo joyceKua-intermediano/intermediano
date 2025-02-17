66f3749928d94acdf0ac18b31c1c684f8ccac957
Popoy-Dev <popoykua28@gmail.com>
2025-02-18 01:00:34 +0800

    CostaRica: Sort in menu

---
+++ app/Filament/Clusters/IntermedianoSRL.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Filament\Clusters;
+
+use Filament\Clusters\Cluster;
+
+class IntermedianoSRL extends Cluster
+{
+    protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
+    protected static ?string $navigationGroup = 'Intermediano Group Companies';
+    protected static ?int $navigationSort = 10;
+
+}
