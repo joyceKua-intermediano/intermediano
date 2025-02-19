7a489cc2a9ef3bdbff89debaff66051c7c8a154b
Popoy-Dev <popoykua28@gmail.com>
2025-02-19 18:40:35 +0800

    Peru: Payroll module

---
+++ app/Filament/Clusters/IntermedianoPeruSAC/Resources/PayrollResource/Pages/EditPayroll.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Clusters\IntermedianoPeruSAC\Resources\PayrollResource\Pages;
+
+use App\Filament\Clusters\IntermedianoPeruSAC\Resources\PayrollResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditPayroll extends EditRecord
+{
+    protected static string $resource = PayrollResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+            Actions\ForceDeleteAction::make(),
+            Actions\RestoreAction::make(),
+        ];
+    }
+    protected function getRedirectUrl(): string {
+        return $this->getResource()::getUrl('index');
+    }
+}
