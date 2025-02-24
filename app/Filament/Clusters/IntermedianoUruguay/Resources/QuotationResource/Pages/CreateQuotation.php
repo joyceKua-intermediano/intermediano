c0dbc699338c309caede05b9327c4e350f590de0
Popoy-Dev <popoykua28@gmail.com>
2025-02-24 21:55:11 +0800

    Uruguay: Resource

---
+++ app/Filament/Clusters/IntermedianoUruguay/Resources/QuotationResource/Pages/CreateQuotation.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Filament\Clusters\IntermedianoUruguay\Resources\QuotationResource\Pages;
+
+use App\Filament\Clusters\IntermedianoUruguay\Resources\QuotationResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateQuotation extends CreateRecord
+{
+    protected static string $resource = QuotationResource::class;
+    protected function getRedirectUrl(): string {
+        return $this->getResource()::getUrl('index');
+    }
+}
