d95717574940ab7410f536b92836e96f0336eafd
Popoy-Dev <popoykua28@gmail.com>
2025-02-24 21:44:12 +0800

    Uruguay: QuotationExport include uruguay

--- app/Exports/QuotationExport.php
+++ app/Exports/QuotationExport.php
@@ -52,6 +52,9 @@ class QuotationExport implements FromView,  WithEvents
             case  $this->record->cluster_name === 'IntermedianoCanada':
                 $exportFile = 'exports.quotations.canada';
                 break;
+            case  $this->record->cluster_name === 'IntermedianoUruguay':
+                $exportFile = 'exports.quotations.uruguay';
+                break;
 
             default:
                 # code...
