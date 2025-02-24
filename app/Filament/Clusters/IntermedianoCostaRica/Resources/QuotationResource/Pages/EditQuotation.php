e9b101966d016e714cfaf172b427e0ca12908506
Popoy-Dev <popoykua28@gmail.com>
2025-02-24 17:47:55 +0800

    Costa Rica: Customer filter and hide country

--- app/Filament/Clusters/IntermedianoCostaRica/Resources/QuotationResource/Pages/EditQuotation.php
+++ app/Filament/Clusters/IntermedianoCostaRica/Resources/QuotationResource/Pages/EditQuotation.php
@@ -18,4 +18,7 @@ class EditQuotation extends EditRecord
             Actions\RestoreAction::make(),
         ];
     }
+    protected function getRedirectUrl(): string {
+        return $this->getResource()::getUrl('index');
+    }
 }
