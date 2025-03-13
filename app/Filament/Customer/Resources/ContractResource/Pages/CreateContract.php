73bb3b5ca8109587fb5d866b8523aa81e0a61d52
Popoy-Dev <popoykua28@gmail.com>
2025-03-14 00:01:24 +0800

    Contract: Contract Resource for customer hub

---
+++ app/Filament/Customer/Resources/ContractResource/Pages/CreateContract.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Customer\Resources\ContractResource\Pages;
+
+use App\Filament\Customer\Resources\ContractResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateContract extends CreateRecord
+{
+    protected static string $resource = ContractResource::class;
+}
