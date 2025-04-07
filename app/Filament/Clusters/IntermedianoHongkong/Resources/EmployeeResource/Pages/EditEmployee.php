27ca4734cc2eeb7e498134ffe7cbb17622f42c82
Popoy-Dev <popoykua28@gmail.com>
2025-04-07 21:30:55 +0800

    HongKong Employee: Resource CRUD

---
+++ app/Filament/Clusters/IntermedianoHongkong/Resources/EmployeeResource/Pages/EditEmployee.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Clusters\IntermedianoHongkong\Resources\EmployeeResource\Pages;
+
+use App\Filament\Clusters\IntermedianoHongkong\Resources\EmployeeResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditEmployee extends EditRecord
+{
+    protected static string $resource = EmployeeResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+    protected function getRedirectUrl(): string {
+        return $this->getResource()::getUrl('index');
+    }
+}
