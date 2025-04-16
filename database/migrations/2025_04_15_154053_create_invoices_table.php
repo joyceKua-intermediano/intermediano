018739de454a71d06ae78c313984b077e2dbc847
Popoy-Dev <popoykua28@gmail.com>
2025-04-16 20:42:37 +0800

    Invoice Hongkong: Model and migration

---
+++ database/migrations/2025_04_15_154053_create_invoices_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('employee_id')->constrained()->cascadeOnDelete(); 
+            $table->foreignId('company_id')->constrained()->cascadeOnDelete(); 
+            $table->date(column: 'invoice_date');
+            $table->json('invoice_items');
+            $table->string('cluster_name');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
