d4f775d04829c8f1c75b91b11d03daded646ded7
Popoy-Dev <popoykua28@gmail.com>
2025-04-28 18:42:20 +0800

    Canada: Employee Resource

---
+++ app/Filament/Clusters/IntermedianoCanada/Resources/EmployeeResource/Pages/CreateEmployee.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Filament\Clusters\IntermedianoCanada\Resources\EmployeeResource\Pages;
+
+use App\Filament\Clusters\IntermedianoCanada\Resources\EmployeeResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateEmployee extends CreateRecord
+{
+    protected static string $resource = EmployeeResource::class;
+    protected function getRedirectUrl(): string {
+        return $this->getResource()::getUrl('index');
+    }
+}
