afc5169641a5c782c141362d43121e19ad01bd46
Popoy-Dev <popoykua28@gmail.com>
2025-05-05 21:08:29 +0800

    Peru: Employee Resource

---
+++ app/Filament/Clusters/IntermedianoPeruSAC/Resources/EmployeeResource/Pages/EditEmployee.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Clusters\IntermedianoPeruSAC\Resources\EmployeeResource\Pages;
+
+use App\Filament\Clusters\IntermedianoPeruSAC\Resources\EmployeeResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditEmployee extends EditRecord
+{
+    protected static string $resource = EmployeeResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+    protected function getRedirectUrl(): string {
+        return $this->getResource()::getUrl('index');
+    }
+    
+}
