bac146b9863106309500abb21d7cc495217e92dd
Popoy-Dev <popoykua28@gmail.com>
2025-05-09 23:09:10 +0800

    Ecuador: Timesheet

---
+++ app/Filament/Clusters/IntermedianoEcuadorSAS/Resources/TimesheetResource/Pages/EditTimesheet.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Clusters\IntermedianoEcuadorSAS\Resources\TimesheetResource\Pages;
+
+use App\Filament\Clusters\IntermedianoEcuadorSAS\Resources\TimesheetResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditTimesheet extends EditRecord
+{
+    protected static string $resource = TimesheetResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
