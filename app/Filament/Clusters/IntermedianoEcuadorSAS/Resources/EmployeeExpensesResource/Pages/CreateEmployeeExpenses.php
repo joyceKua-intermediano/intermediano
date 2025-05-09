0ab825e70d3e412c711437f50dee17efea5df900
Popoy-Dev <popoykua28@gmail.com>
2025-05-09 23:11:21 +0800

    Ecuador: Employee Expenses

---
+++ app/Filament/Clusters/IntermedianoEcuadorSAS/Resources/EmployeeExpensesResource/Pages/CreateEmployeeExpenses.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Clusters\IntermedianoEcuadorSAS\Resources\EmployeeExpensesResource\Pages;
+
+use App\Filament\Clusters\IntermedianoEcuadorSAS\Resources\EmployeeExpensesResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateEmployeeExpenses extends CreateRecord
+{
+    protected static string $resource = EmployeeExpensesResource::class;
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
