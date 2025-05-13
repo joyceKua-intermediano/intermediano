14b5290a1951dd7f24a6c93d8bd1a933888e4f75
Popoy-Dev <popoykua28@gmail.com>
2025-05-14 00:29:23 +0800

    Costa Rica: EmployeeContract resource

---
+++ app/Filament/Clusters/IntermedianoCostaRica/Resources/EmployeeContractResource/Pages/EditEmployeeContract.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Clusters\IntermedianoCostaRica\Resources\EmployeeContractResource\Pages;
+
+use App\Filament\Clusters\IntermedianoCostaRica\Resources\EmployeeContractResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditEmployeeContract extends EditRecord
+{
+    protected static string $resource = EmployeeContractResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
