08b406de9110f745a523ae5a7f415ebcea796ebf
Popoy-Dev <popoykua28@gmail.com>
2025-05-16 23:34:04 +0800

    Mexico: Employee Resource

---
+++ app/Filament/Clusters/IntermedianoMexicoSC/Resources/EmployeeResource/Pages/EditEmployee.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Clusters\IntermedianoMexicoSC\Resources\EmployeeResource\Pages;
+
+use App\Filament\Clusters\IntermedianoMexicoSC\Resources\EmployeeResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditEmployee extends EditRecord
+{
+    protected static string $resource = EmployeeResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
