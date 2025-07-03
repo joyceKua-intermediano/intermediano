8d36bdfc42b8f52f81580b4fc3ae6bc194716b34
Popoy-Dev <popoykua28@gmail.com>
2025-07-03 08:22:22 +0800

    Feature_Hongkong_admin_signature: Hongkong Balde employee

--- resources/views/pdf/contract/hongkong/employee.blade.php
+++ resources/views/pdf/contract/hongkong/employee.blade.php
@@ -43,7 +43,15 @@ $employeeEndDate = \Carbon\Carbon::parse($record->end_date)->format('d m Y');
 $employeeGrossSalary = $record->gross_salary;
 $employeeJobDescription = $record->job_description;
 $signaturePath = 'signatures/employee_' . $record->employee_id . '.webp';
-$signatureExists = Storage::disk('public')->exists($signaturePath);
+$signatureExists = Storage::disk('private')->exists($signaturePath);
+$adminSignaturePath = 'signatures/admin/admin_' . $record->id . '.webp';
+$adminSignatureExists = Storage::disk('private')->exists($adminSignaturePath);
+$adminSignedBy = $record->user->name;
+
+$user = auth()->user();
+$isAdmin = $user instanceof \App\Models\User;
+$type = $isAdmin ? 'admin' : 'employee';
+
 @endphp
 
 <style>
@@ -80,12 +88,12 @@ $signatureExists = Storage::disk('public')->exists($signaturePath);
     <main class="main-container {{  $is_pdf ? 'is-pdf' : 'non-pdf'  }}">
         <h4 style='text-align:center; text-decoration: underline; margin: 20px 0px'> SERVICE AGREEMENT</h4>
 
-        <p> This Service Agreement (the “Agreement”) is made on [DATE] (the “Effective Date”), by
+        <p> This Service Agreement (the "Agreement") is made on [DATE] (the "Effective Date"), by
             and between INTERMEDIANO HONG KONG LIMITED, a Hong Kong company, enrolled
             under the registration number 77682459, located at Flat A11/F. Cheung Lung Ind Bldg 10
-            CheungYeeST,CheungShaWan,HongKong,herein referred to simply as (the“Provider”);
+            CheungYeeST,CheungShaWan,HongKong,herein referred to simply as (the"Provider");
             AND <b>{{ $companyName }}</b>, a company duly incorporated with {{ $companyTaxId }} under the laws of {{ $companyCountry }} and holding offices at {{ $companyAddress }}, {{ $companyCity }}, {{ $companyState }}, {{ $companyCountry }},
-            herein referred to simply as (the “Provider”).</p>
+            herein referred to simply as (the "Provider").</p>
 
 
         <p><b>WHEREAS</b> Provider provides certain payroll, tax, and human resource services globally
@@ -100,13 +108,13 @@ $signatureExists = Storage::disk('public')->exists($signaturePath);
 
         <p>Provider and Customer hereinafter jointly referred to as "Parties" and individually a "Party";</p>
 
-        <p> The Parties decide to enter into the present Services Agreement (“Agreement”), which shall be governed by the following terms and conditions:</p>
+        <p> The Parties decide to enter into the present Services Agreement ("Agreement"), which shall be governed by the following terms and conditions:</p>
 
 
         <p class='clause-header'><b><span>PURPOSE</span></b></p>
         <p><b>Service Offerings.</b> Provider shall provide to Customer the services of payroll, consulting and
-            HR attached hereto as Schedule A (the “Schedule A”) and incorporated herein
-            (collectively, the “Services”), during the Term (defined in Section VIII) subject to the terms
+            HR attached hereto as Schedule A (the "Schedule A") and incorporated herein
+            (collectively, the "Services"), during the Term (defined in Section VIII) subject to the terms
             and conditions of this Agreement.</p>
 
         <p class='clause-header'><b><span> PROVIDER RESPONSIBILITIES</span></b></p>
@@ -139,7 +147,7 @@ $signatureExists = Storage::disk('public')->exists($signaturePath);
         <p>Notwithstanding the other obligations under this Agreement, the Customer, hereby
             undertakes to:</p>
 
-        <p> to process the monthly payment to the Provider set forth in <b>Schedule B</b> (the “Schedule B”),
+        <p> to process the monthly payment to the Provider set forth in <b>Schedule B</b> (the "Schedule B"),
             following strictly the local labour legislation, considering where the service is being
             provided to supply the technical information required for the Services to be performed;
         </p>
@@ -153,8 +161,8 @@ $signatureExists = Storage::disk('public')->exists($signaturePath);
         <p> Both parties agree to endeavour to take all reasonable measures to keep in confidence
             the execution, terms and conditions as well as performance of this Agreement, and the
             confidential data and information of any party that another party may know or access
-            during performance of this Agreement (hereinafter referred to as “Confidential
-            Information”), and shall not disclose, make available or assign such Confidential
+            during performance of this Agreement (hereinafter referred to as "Confidential
+            Information"), and shall not disclose, make available or assign such Confidential
             Information to any third party without the prior written consent of the party providing the
             information.</p>
         <p> Information received by aParty which: (i) is information of general or public knowledge; (ii)
@@ -307,11 +315,22 @@ $signatureExists = Storage::disk('public')->exists($signaturePath);
                     <h4>INTERMEDIANO HONG KONG LIMITED</h4>
 
                     <div style="text-align: center; position: relative; height: 100px;">
-                        <img src="{{ $is_pdf ? public_path('images/fernando_signature.png') : asset('images/fernando_signature.png') }}" alt="Signature" style="height: 50px; position: absolute; bottom: 25%; left: 50%; transform: translateX(-50%);">
+                        @if($adminSignatureExists)
+                        <img src="{{ $is_pdf 
+        ? storage_path('app/private/signatures/admin/admin_' . $record->id . '.webp') 
+        : url('/signatures/' . $type. '/' . $record->id . '/admin') }}" alt="Signature" style="height: 50px; position: absolute; bottom: 25%; left: 50%; transform: translateX(-50%);" />
+
 
                         <div style="width: 70%; border-bottom: 1px solid black; position: absolute; bottom: 24px; left: 50%; transform: translateX(-50%); z-index: 100;"></div>
 
-                        <p style="position: absolute; bottom: -10px; left: 50%; transform: translateX(-50%);">Fernando Gutierrez</p>
+
+                        <p style="position: absolute; bottom: -25px;width: 100%; left: 50%; transform: translateX(-50%); text-align: center;">{{ \Carbon\Carbon::parse($record->admin_signed_contract)->format('d/m/Y h:i A') }}</p>
+                        @else
+                        <img src="{{ $is_pdf ? public_path('images/blank_signature.png') : asset('images/blank_signature.png') }}" alt="Signature" style="height: 10px; margin-top: 40px; z-index: 1000; position: absolute; bottom: 25%; left: 50%; transform: translateX(-50%);">
+                        @endif
+                        <p style="position: absolute; bottom: -10px; left: 50%; transform: translateX(-50%);">{{ $adminSignedBy }}</p>
+                        <div style="width: 70%; border-bottom: 1px solid black; position: absolute; bottom: 24px; left: 50%; transform: translateX(-50%); z-index: 100;"></div>
+
                     </div>
 
 
@@ -320,7 +339,9 @@ $signatureExists = Storage::disk('public')->exists($signaturePath);
                     <h4>{{ $companyName }}</h4>
                     <div style="display: inline-block; position: relative; height: 100px; width: 100%;">
                         @if($signatureExists)
-                        <img src="{{ $is_pdf ? storage_path('app/public/signatures/employee_' . $record->employee_id . '.webp') : asset('storage/signatures/employee_' . $record->employee_id . '.webp') }}" alt="Signature" style="height: 50px; position: absolute; bottom: 25%; left: 50%; transform: translateX(-50%);">
+                        <img src="{{ $is_pdf
+            ? storage_path('app/private/signatures/employee_' . $record->employee_id . '.webp')
+            : url('/signatures/' . $type. '/' . $record->employee_id . '/employee') }}" alt="Employee Signature" style="height: 50px; position: absolute; bottom: 25%; left: 50%; transform: translateX(-50%);">
 
                         <div style="width: 70%; border-bottom: 1px solid black; position: absolute; bottom: 24px; left: 50%; transform: translateX(-50%); z-index: 100;"></div>
 
@@ -328,7 +349,7 @@ $signatureExists = Storage::disk('public')->exists($signaturePath);
                         @else
                         <img src="{{ $is_pdf ? public_path('images/blank_signature.png') : asset('images/blank_signature.png') }}" alt="Signature" style="height: 10px; margin-top: 40px; z-index: 1000; position: absolute; bottom: 25%; left: 50%; transform: translateX(-50%);">
                         @endif
-                        <p style="position: absolute; bottom: -10px; left: 50%; transform: translateX(-50%);">{{ $customerName }}</p>
+                        <p style="position: absolute; bottom: -10px; left: 50%; transform: translateX(-50%);">{{ $employeeName }}</p>
 
                     </div>
 
