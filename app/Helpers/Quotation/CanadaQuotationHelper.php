2a82fa2405ab96ac3c6ee55a49e11bda205066dd
Popoy-Dev <popoykua28@gmail.com>
2025-07-31 00:16:15 +0800

    Canada/Feature: Provision payment, balance

--- app/Helpers/Quotation/CanadaQuotationHelper.php
+++ app/Helpers/Quotation/CanadaQuotationHelper.php
@@ -1,8 +1,13 @@
 <?php
 
 if (!function_exists('calculateCanadaQuotation')) {
-    function calculateCanadaQuotation($record, $previousMonthRecord)
+    function calculateCanadaQuotation($record, $previousRecords)
     {
+        $previousVacation = 0;
+        $previousIndemnization = 0;
+
+        $previousPaidVacation = 0;
+        $previousPaidIndemnization = 0;
 
         $grossSalary = $record->gross_salary;
 
@@ -17,7 +22,7 @@ if (!function_exists('calculateCanadaQuotation')) {
             $record->internet_allowance;
 
         $canadanPension = $totalGrossIncome * .0595;
-        $employmentInsurance = $totalGrossIncome * .0232 ;
+        $employmentInsurance = $totalGrossIncome * .0232;
         $employerHealthTax = $totalGrossIncome * 0.0195;
         $workerCompensation = $totalGrossIncome * .01;
         $glInsurance = $totalGrossIncome * .01;
@@ -35,32 +40,63 @@ if (!function_exists('calculateCanadaQuotation')) {
         $provisionsTotal = $vacation + $indemnification;
 
         // accumulated provision
-        if ($previousMonthRecord) {
-            $previousMonthGrossIncome = $previousMonthRecord->gross_salary +
-                $previousMonthRecord->bonus +
-                $previousMonthRecord->home_allowance +
-                $previousMonthRecord->transport_allowance +
-                $previousMonthRecord->medical_allowance +
-                $previousMonthRecord->legal_grafication +
-                $previousMonthRecord->internet_allowance;
-        } else {
-            $previousMonthGrossIncome = 0;
-        };
-        $accumulatedVacation = (0.0416 * $previousMonthGrossIncome) + $vacation;
-        $accumulatedIndemnification = (0.0833 * $previousMonthGrossIncome) + $indemnification;
+        if ($previousRecords && $previousRecords->count()) {
+            foreach ($previousRecords as $item) {
+                $gross = $item->gross_salary +
+                    $item->bonus +
+                    $item->home_allowance +
+                    $item->transport_allowance +
+                    $item->medical_allowance +
+                    $item->internet_allowance;
 
-        $accumulatedProvisionsTotal = $accumulatedVacation + $accumulatedIndemnification;
+                $previousVacation += 0.0416 * $gross;
+                $previousIndemnization += 0.0833 * $gross;
+
+
+                // Payments
+                $previousPaidVacation += $item->paymentProvisions->where('provisionType.name', 'Vacation')->sum('amount');
+                $previousPaidIndemnization += $item->paymentProvisions->where('provisionType.name', 'Indemnification')->sum('amount');
+
+            }
+        }
 
         // end of accumulated provision
 
         $subTotalGrossPayroll = $totalGrossIncome + $provisionsTotal + $payrollCostsTotal;
-        $fee = $record->is_fix_fee ? $record->fee * $record->exchange_rate : $subTotalGrossPayroll * ($record->fee / 100);
+        $fee = $record->is_fix_fee ? $record->fee * $record->exchange_rate : $totalGrossIncome * ($record->fee / 100);
         $bankFee = $record->bank_fee * $record->exchange_rate;
-        $subTotal = $subTotalGrossPayroll + $fee ;
+        $subTotal = $subTotalGrossPayroll + $fee;
         $municipalTax = 0 * $subTotal;
         $servicesTaxes = $subTotal * 0.19;
         $totalInvoice = $subTotal + $bankFee;
 
+        // current payment
+
+        $currentPaidVacation = $record->paymentProvisions
+            ->where('provisionType.name', 'Vacation')
+            ->sum('amount');
+
+        $currentPaidIndemnization = $record->paymentProvisions
+            ->where('provisionType.name', 'Indemnification')
+            ->sum('amount');
+
+
+        // All-time payments
+        $totalPaidVacation = $previousPaidVacation + $currentPaidVacation;
+        $totalPaidIndemnization = $previousPaidIndemnization + $currentPaidIndemnization;
+
+        // accumulated
+        $accumulatedVacation = ($previousVacation + $vacation) - $previousPaidVacation;
+        $accumulatedIndemnification = ($previousIndemnization + $indemnification) - $previousPaidIndemnization;
+
+
+        $accumulatedProvisionsTotal = $accumulatedIndemnification + $accumulatedVacation;
+
+        // Balances
+        $balanceVacation = $accumulatedVacation - $currentPaidVacation;
+        $balanceIndemnization = $accumulatedIndemnification - $currentPaidIndemnization;
+
+        $balanceProvisionsTotal = $balanceVacation + $balanceIndemnization;
         return [
             'grossSalary' => $grossSalary,
             'totalGrossIncome' => $totalGrossIncome,
@@ -80,10 +116,16 @@ if (!function_exists('calculateCanadaQuotation')) {
             'glInsurance' => $glInsurance,
             'vacation' => $vacation,
             'indemnification' => $indemnification,
-            'previousMonthGrossIncome' => $previousMonthGrossIncome,
+            'hasPreviousRecords' => !empty($previousRecords),
+
             'accumulatedVacation' => $accumulatedVacation,
             'accumulatedIndemnification' => $accumulatedIndemnification,
             'accumulatedProvisionsTotal' => $accumulatedProvisionsTotal,
+
+            'paymentProvisions' => $record->paymentProvisions,
+            'balanceIndemnization' => $balanceIndemnization,
+            'balanceVacation' => $balanceVacation,
+            'balanceProvisionsTotal' => $balanceProvisionsTotal,
         ];
     }
 }
