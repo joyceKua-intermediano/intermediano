a801cd4378dafe3a26a23d56f936f6a56bbac44b
Popoy-Dev <popoykua28@gmail.com>
2025-08-27 20:41:33 +0800

    Brazil/Featur: Payslips add employee and time period

--- app/Models/Payslip.php
+++ app/Models/Payslip.php
@@ -8,7 +8,13 @@ class Payslip extends Model
 {
     protected $fillable = [
         'cluster',
+        'employee_id',
+        'payslip_period',
         'file_name',
         'file_path',
     ];
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
 }
