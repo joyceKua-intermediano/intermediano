2549939d8ec3d664c050d662dac2234d35ec61fd
Popoy-Dev <popoykua28@gmail.com>
2025-10-14 02:26:33 +0800

    Canada/Payslip: Fix form for uploading payslip

--- app/Filament/Clusters/IntermedianoCanada/Resources/PayslipResource/Pages/ListPayslips.php
+++ app/Filament/Clusters/IntermedianoCanada/Resources/PayslipResource/Pages/ListPayslips.php
@@ -8,8 +8,11 @@ use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\Select;
 use Illuminate\Support\Facades\Storage;
 use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Forms\Components\DatePicker;
 
 class ListPayslips extends ListRecords
 {
@@ -24,6 +27,16 @@ class ListPayslips extends ListRecords
                 ->color('primary')
                 ->icon('heroicon-o-arrow-up-tray')
                 ->form([
+                    Select::make('employee_id')
+                        ->relationship('employee', 'name', fn(Builder $query) => $query->where('company', 'IntermedianoCanada'))
+                        ->required(),
+                    DatePicker::make('payslip_period')
+                        ->displayFormat('Y-m')
+                        ->placeholder('Payslip Period')
+                        ->extraInputAttributes(['type' => 'month'])
+
+                        ->native(),
+
                     FileUpload::make('file_path')
                         ->label('Payslip File')
                         ->disk('r2') // directly store in your R2 disk
@@ -35,18 +48,20 @@ class ListPayslips extends ListRecords
                         ->visibility('public')
                         ->required(),
                     Hidden::make('cluster')
-                        ->default('intermedianoCanada'),
+                        ->default('IntermedianoDoBrasilLtda'),
                 ])
                 ->action(function (array $data) {
                     $cluster = $data['cluster'];
-                    $filePath = $data['file_path']; // already uploaded to R2
+                    $filePath = $data['file_path']; 
                     $fileName = basename($filePath);
                     $newPath = "payslips/{$cluster}/{$fileName}";
                     Storage::disk('r2')->move($filePath, $newPath);
                     Payslip::create([
+                        'employee_id' => $data['employee_id'],
+                        'payslip_period' => $data['payslip_period'],
                         'cluster' => $cluster,
-                        'file_name' => $fileName, // readable name
-                        'file_path' => $filePath, // full R2 object key
+                        'file_name' => $fileName,
+                        'file_path' => $newPath,
                     ]);
                 }),
         ];
